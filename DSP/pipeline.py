0770c75a991b29d853dfd51e80adfaff6ea6adf5
2020e <2020edriessen@gmail.com>
2024-03-08 15:39:15 -0600

    Attemp to multiprocess. TypeError: cannot pickle '_thread.lock' object WIP

--- DSP/pipeline.py
+++ DSP/pipeline.py
@@ -1,4 +1,4 @@
-import threading
+import multiprocessing as mp
 from DSP import plotters, filters, recorders, direction_of_arrival
 
 
@@ -7,12 +7,13 @@ class AudioPipeline:
                  recorder: recorders,
                  filters: list[filters],
                  dsp_algorithms: list[direction_of_arrival],
-                 plotters: list[plotters]):
+                 plotters: list[plotters],
+                 num_processes: int = 4):
         self.recorder = recorder
         self.filters = filters
         self.dsp_algorithms = dsp_algorithms
         self.plotters = plotters
-        self.thread = threading.Thread(target=self._update)
+        self.processes = [mp.Process(target=self._update) for i in range(num_processes)]
 
     def flowpath(self):
         """     EXAMPLE
@@ -28,9 +29,8 @@ class AudioPipeline:
         for algo in self.dsp_algorithms:
             results.append(algo.process())
 
-        # Plot results
-        for result, plot in zip(self.plotters, results):
-            plot.setData(result)
+        # DO NOT PROCESS RESULTS IN HERE!!!
+        # rather, call self.sink()
         """
         raise NotImplementedError   # subclass must implement this
 
@@ -41,8 +41,11 @@ class AudioPipeline:
 
     def start(self):
         self.recorder.start()
-        self.thread.start()
+        for process in self.processes:
+            process.start()
         self.plotters[0].show()
 
     def stop(self):
         self.recorder.stop()
+        for process in self.processes:
+            process.terminate()
