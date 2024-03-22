a0f12a49fd6fe998f58bacb4b913f8bd3d3f404a
2020e <2020edriessen@gmail.com>
2024-03-22 13:19:23 -0500

    added documentation

--- DSP/spectral.py
+++ DSP/spectral.py
@@ -3,11 +3,24 @@ from Management import pipeline
 import scipy.signal as sig
 
 class Periodogram(pipeline.Stage):
+    """
+    Estimates power spectral density for each channel using a periodogram. Pushes a tuple of (f, pxx)
+    """
     def __init__(self, samplerate=44100, queue_size=4, destinations=None):
+        """
+        Initializes the Periodogram
+        :param samplerate: The samplerate of the source
+        :param queue_size: The size of the input queue
+        :param destinations: Where to push (f, pxx)
+        """
         super().__init__(1, queue_size, destinations)
         self.samplerate = samplerate
 
     def run(self):
+        """
+        Continuously applies periodogram to each channel of the input data. Pushes (f, pxx) to destinations
+        :return:
+        """
         while True:
             data = self.input_queue_get()[0]
             f = np.zeros_like(data)
