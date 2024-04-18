1a35f06af47067b01bf4018f55c549f6275e0223
2020e <2020edriessen@gmail.com>
2024-04-18 10:59:44 -0500

    Added exporters

--- Scripts/three_dim_music_simulation.py
+++ Scripts/three_dim_music_simulation.py
@@ -1,7 +1,6 @@
 import DSP
 import Geometry
 import numpy as np
-from itertools import product
 
 
 def main():
@@ -12,40 +11,26 @@ def main():
     wave_number = 2
     speed_of_sound = 343
 
-    # ELEMENTS
-    # sphere
-    elements = [Geometry.Element(Geometry.spherical_to_cartesian(np.array([1, theta, phi]))) for (theta, phi)
-                in product(np.linspace(0, np.pi, 5), np.linspace(0, 2 * np.pi, 10))]
-
-    # Box
-    # elements = [Geometry.Element([x, y, z]) for (x, y, z) in
-    #             product(np.arange(0, 4, 0.5), np.arange(0, 4, 0.5), np.arange(0, 4, 0.5))]
-
-    # +
-    # elements = [Geometry.Element([-1.25, 0, 0]),
-    #             Geometry.Element([-0.75, 0, 0]),
-    #             Geometry.Element([-0.25, 0, 0]),
-    #             Geometry.Element([0.25, 0, 0]),
-    #             Geometry.Element([0.75, 0, 0]),
-    #             Geometry.Element([1.25, 0, 0]),
-    #             Geometry.Element([0, -1.25, 0]),
-    #             Geometry.Element([0, -0.75, 0]),
-    #             Geometry.Element([0, -0.25, 0]),
-    #             Geometry.Element([0, 0.25, 0]),
-    #             Geometry.Element([0, 0.75, 0]),
-    #             Geometry.Element([0, 1.25, 0]),
-    #             Geometry.Element([0, 0, 0.25]),
-    #             Geometry.Element([0, 0, 0.75]),
-    #             Geometry.Element([0, 0, 1.25])]
+    elements = [Geometry.Element([-1.25, 0, 0]),
+                Geometry.Element([-0.75, 0, 0]),
+                Geometry.Element([-0.25, 0, 0]),
+                Geometry.Element([0.25, 0, 0]),
+                Geometry.Element([0.75, 0, 0]),
+                Geometry.Element([1.25, 0, 0]),
+                Geometry.Element([0, -1.25, 0]),
+                Geometry.Element([0, -0.75, 0]),
+                Geometry.Element([0, -0.25, 0]),
+                Geometry.Element([0, 0.25, 0]),
+                Geometry.Element([0, 0.75, 0]),
+                Geometry.Element([0, 1.25, 0]),
+                Geometry.Element([0, 0, 0.25]),
+                Geometry.Element([0, 0, 0.75]),
+                Geometry.Element([0, 0, 1.25])]
 
     wave_vectors = [
         Geometry.WaveVector([wave_number * 1.00, np.pi / 2.5, np.pi / 2.5], speed_of_sound),
         Geometry.WaveVector([wave_number * 1.02, np.pi / 3.5, np.pi / 3.5], speed_of_sound),
     ]
-
-    # Print frequencies for debugging
-    for vect in wave_vectors:
-        print(vect.frequency)
 
     # Recorder to get data
     recorder = DSP.AudioSimulator(
