187c12f9769c35f03a56f0cd44c45e54727271d7
2020e <2020edriessen@gmail.com>
2024-04-24 14:55:27 -0500

    addec calculate_cov_matrix. Modified calculate_noise_subspace to reflect this and use this

--- AccCam/direction_of_arrival/estimate.py
+++ AccCam/direction_of_arrival/estimate.py
@@ -3,18 +3,23 @@ from AccCam.direction_of_arrival.geometry import Structure
 from abc import ABC, abstractmethod
 
 
-def find_noise_subspace(data: np.ndarray, num_sources: int) -> np.ndarray:
+def calculate_covariance(data: np.ndarray) -> np.ndarray:
+    """
+    :param data: The signal matrix to find the covariance of
+    :return: The covariance matrix of the signal matrix
+    """
+    return np.cov(data.T)
+
+
+def calculate_noise_subspace(cov_data: np.ndarray, num_sources: int) -> np.ndarray:
     """
     Find the noise subspace of a provided signal
-    :param data: The signal to find the noise subspace of
+    :param cov_data: The covariance matrix to find the noise subspace of
     :param num_sources: The number of sources in the environment
     :return:
     """
-    # Calculate the covariance matrix
-    rx = np.cov(data.T)
-
     # Decompose into eigenvalues and vectors
-    eigvals, eigvecs = np.linalg.eigh(rx)
+    eigvals, eigvecs = np.linalg.eigh(cov_data)
 
     noise_subspace = eigvecs[:, np.argsort(eigvals)][:, :-num_sources]
     return noise_subspace
@@ -69,7 +74,7 @@ class BartlettBeamformer(Estimator):
         super().__init__(structure)
 
     def process(self, data: np.ndarray) -> np.ndarray:
-        cov_matrix = np.cov(data.T)
+        cov_matrix = calculate_covariance(data)
         beamformed_data = np.sum(self.structure.steering_matrix.conj().T *
                                  (cov_matrix @ self.structure.steering_matrix).T, axis=1).real
 
@@ -89,7 +94,7 @@ class MVDRBeamformer(Estimator):
         super().__init__(structure)
 
     def process(self, data: np.ndarray) -> np.ndarray:
-        cov_matrix = np.cov(data.T)
+        cov_matrix = calculate_covariance(data)
         beamformed_data = 1 / np.sum(self.structure.steering_matrix.conj().T *
                                      np.linalg.lstsq(
                                          cov_matrix,
@@ -113,8 +118,11 @@ class Music(Estimator):
         self.num_sources = num_sources
 
     def process(self, data: np.ndarray) -> np.ndarray:
+        # Calculate covaraince
+        cov_matrix = calculate_covariance(data)
+
         # Get noise subspace:
-        noise_subspace = find_noise_subspace(data, self.num_sources)
+        noise_subspace = calculate_noise_subspace(cov_matrix, self.num_sources)
 
         # Compute the music spectrum. Use np.sum to take several samples into one result
         music_spectrum = 1 / np.sum(np.abs(self.structure.steering_matrix.conj().T @ noise_subspace) ** 2, axis=1)
